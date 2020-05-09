4a7983ca1c2b7a2737611bcc56626273fe6bff6a
Jeff King <jazfunk@gmail.com>
2020-05-09 10:19:37 -0400

    Moving ball, bouncing off each canvas edge.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let canvas;
 let canvasContext;
 let ballX = 50;
+let ballY = 50;
 let ballSpeedX = 15;
+let ballSpeedY = 4;
 
 window.onload = function () {
   canvas = document.getElementById("gameCanvas");
@@ -16,24 +18,35 @@ window.onload = function () {
 
 function moveEverything() {
   ballX += ballSpeedX;
+  ballY += ballSpeedY;
+  if (ballX < 0) {
+    ballSpeedX = -ballSpeedX;
+  }
   if (ballX > canvas.width) {
     ballSpeedX = -ballSpeedX;
   }
-
+  if (ballY < 0) {
+    ballSpeedY = -ballSpeedY;
+  }
+  if (ballY > canvas.height) {
+    ballSpeedY = -ballSpeedY;
+  }
 }
 
 function drawEverything() {
-  canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  colorRect(0, 0, canvas.width, canvas.height, "black");
+  colorRect(0, 200, 10, 100, "white");
+  colorCircle(ballX, ballY, 10, "white");
+}
 
-  canvasContext.fillStyle = "white";
-  canvasContext.fillRect(0, 200, 10, 100);
+function colorCircle(centerX, centerY, radius, drawColor) {
+  canvasContext.fillStyle = drawColor;
+  canvasContext.beginPath();
+  canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+  canvasContext.fill();
+}
 
-  canvasContext.fillStyle = "red";
-  canvasContext.fillRect(200, 200, 50, 25);
-
-  const canvasMiddle = canvas.width / 2;
-
-  canvasContext.fillStyle = "green";
-  canvasContext.fillRect(ballX, 100, 10, 10);
+function colorRect(leftX, topY, width, height, drawColor) {
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect(leftX, topY, width, height);
 }
