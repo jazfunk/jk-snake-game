dfcc8a88794cfeacb5e5b0b902253bdffe905933
Jeff King <jazfunk@gmail.com>
2020-05-13 12:29:51 -0400

    Control of snakehead complete except for rigth side.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let canvas;
 let canvasDisplay;
 
 let snakeHeadSize = 20;
-let snakeX = 40;
+let snakeX = 20;
 let snakeY = 40;
-let snakeSpeedX = 10;
-let snakeSpeedY = 10;
+let snakeSpeedX = 20;
+let snakeSpeedY = 20;
 
 let myTimer;
 
@@ -16,57 +16,62 @@ let downPressed = false;
 
 document.addEventListener("keydown", keyDownHandler, false);
 
+function clearSnakeDirectionValues() {
+  rightPressed = false;
+  leftPressed = false;
+  upPressed = false;
+  downPressed = false;
+}
+
 function keyDownHandler(e) {
-  let framesPerSecond = 30;
+  e.preventDefault();
+
+  clearSnakeDirectionValues();
+  clearInterval(myTimer);
+
+  let framesPerSecond = 10;
 
   if (e.key === "Right" || e.key === "ArrowRight") {
-    
-    clearInterval(myTimer);
-    
     rightPressed = true;
-    
-    myTimer = setInterval(() => {
-      // drawGameObjects();
-      moveSnakeHorizontal();      
-    }, 1000 / framesPerSecond);
-    // moveSnakeHorizontal();
-  }
-  if (e.key === "Left" || e.key === "ArrowLeft") {
-    
-    clearInterval(myTimer);
 
+    myTimer = setInterval(() => {
+      drawGameObjects();
+      moveSnakeHorizontal();
+    }, 1000 / framesPerSecond);
+  }
+
+  if (e.key === "Left" || e.key === "ArrowLeft") {
     leftPressed = true;
 
     myTimer = setInterval(() => {
       drawGameObjects();
-      moveSnakeHorizontal();      
+      moveSnakeHorizontal();
     }, 1000 / framesPerSecond);
-    // moveSnakeHorizontal();
   }
 
   if (e.key === "Up" || e.key === "ArrowUp") {
     upPressed = true;
-    // moveSnakeVertical();
+
+    myTimer = setInterval(() => {
+      drawGameObjects();
+      moveSnakeVertical();
+    }, 1000 / framesPerSecond);
   }
 
   if (e.key === "Down" || e.key === "ArrowDown") {
     downPressed = true;
-    // moveSnakeVertical();
+
+    myTimer = setInterval(() => {
+      drawGameObjects();
+      moveSnakeVertical();
+    }, 1000 / framesPerSecond);
   }
 }
 
 window.onload = () => {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
-
-  // let framesPerSecond = 30;
-  // myTimer = setInterval(() => {
-  //   drawGameObjects();
-  //   moveSnakeHorizontal();
-  //   moveSnakeVertical();
-  // }, 1000 / framesPerSecond);
 };
-
 
 function createRectangle(leftX, topY, width, height, drawColor) {
   canvasContext.fillStyle = drawColor;
@@ -74,11 +79,12 @@ function createRectangle(leftX, topY, width, height, drawColor) {
 }
 
 function drawGrid() {
-  let gridColor = "#8EE4AF";
-  for (let row = 0; row < canvas.width; row += 20) {
-    for (let col = 20; col < canvas.width; col += 20) {
-      createRectangle(row, col - 20, 20, 1, gridColor);
-      createRectangle(col, row - 20, 1, 20, gridColor);
+  // let gridColor = "#8EE4AF";
+  let gridColor = "#379683";
+  for (let row = 0; row < canvas.height; row += 20) {
+    for (let col = 0; col < canvas.width; col += 20) {
+      createRectangle(col + 20, row, 1, 20, gridColor);
+      createRectangle(col, row + 20, 20, 1, gridColor);
     }
   }
 }
@@ -87,52 +93,46 @@ function drawGameObjects() {
   // Game background
   createRectangle(0, 0, canvas.width, canvas.height, "#5CDB95");
 
-  // Draw Grid Lines
   drawGrid();
 
   // Header
-  createRectangle(0, 0, canvas.width, 20, "#05386B");
+  // createRectangle(0, 0, canvas.width, 20, "#05386B");
 
   // Snake Head
   createRectangle(snakeX, snakeY, snakeHeadSize, snakeHeadSize, "#05386B");
 }
 
 function moveSnakeHorizontal() {
-  snakeX += snakeSpeedX;
-
-  if (snakeX < 2 || snakeX > canvas.width - 22) {
-    // clearInterval(myTimer);
-    snakeSpeedX = -snakeSpeedX;
+  if (snakeX < 0 || snakeX > canvas.width - 40) {
+    clearInterval(myTimer);
+    clearSnakeDirectionValues();
   }
-
-  // if (snakeX > canvas.width - 22) {
-  //   // clearInterval(myTimer);
-  //   snakeSpeedX = -snakeSpeedX;
-  // }
-
   if (leftPressed) {
-    // snakeX -= snakeSpeedX;
+    if (snakeX >= snakeHeadSize) {
+      snakeX -= snakeSpeedX;
+    }
   }
   if (rightPressed) {
-    // snakeX += snakeSpeedX;
+    if (snakeX <= canvas.width - snakeHeadSize) {
+      snakeX += snakeSpeedX;
+    }
   }
+
 }
 
-function moveSnakeVertical() {
-  if (snakeY < 22) {
-    // clearInterval(myTimer);
-    snakeSpeedY = -snakeSpeedY;
+function moveSnakeVertical() {  
+  if (snakeY < 0 || snakeY > canvas.height - snakeHeadSize) {
+    clearInterval(myTimer);
+    clearSnakeDirectionValues();
   }
-
-  if (snakeY > canvas.height - 22) {
-    // clearInterval(myTimer);
-    snakeSpeedY = -snakeSpeedY;
+  if (downPressed) {
+    if (snakeY < canvas.height - snakeHeadSize) {
+      snakeY += snakeSpeedY;
+    }
   }
-
-  // if (downPressed) {
-  //   snakeY += snakeSpeedY;
-  // }
-  // if (upPressed) {
-  //   snakeY -= snakeSpeedY;
-  // }
+  if (upPressed) {
+    if (snakeY >= snakeHeadSize) {
+      snakeY -= snakeSpeedY;
+    }
+  }
 }
